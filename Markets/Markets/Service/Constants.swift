1c3738078eab763cd3e05ce0e956b3abeed2c7c9
manjeet <manjeet.dhanjal@gmail.com>
2019-10-23 20:44:06 +1100

    Added Alamofire using Swift Package manager Can use the other options for e.g cocoapods and carthage

---
+++ Markets/Markets/Service/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Markets
+//
+//  Created by Manjeet Singh on 23/10/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Service {
+        static let getBitcoinPrices = "https://blockchain.info/ticker"
+    }
+}
